751d83e06633fbdb16fe69fe6dc775fc0829d65e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-04 21:03:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if(array.size == 1)
+    return array[0]
+  end
+  
+  if(array.size == 2)
+    return array.join(" and ")
+  end
 
+  array[array.size - 1] = "and #{array.last}"
+  return array.join(", ")
 end
